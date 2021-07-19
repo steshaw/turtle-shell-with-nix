1337b727ed4b7bc11f2e23cb4553fbb3a1ced289
Steven Shaw <steven@steshaw.org>
2021-07-19 22:41:33 +1000

    Pin nixpkgs

--- bin/shell.nix
+++ bin/shell.nix
@@ -1,14 +1,21 @@
-with (import <nixpkgs> {}).pkgs;
-let pkg = haskellngPackages.callPackage
-            ({ mkDerivation, base, turtle, stdenv }:
-             mkDerivation {
-               pname = "turtle-example";
-               version = "0.0.0.0";
-               src = ./.;
-               isLibrary = false;
-               isExecutable = true;
-               buildDepends = [ base turtle ];
-               license = stdenv.lib.licenses.bsd2;
-             }) {};
-in
-  pkg.env
+let
+  hostPkgs = import <nixpkgs> { };
+  pinnedPkgs = hostPkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "60accda79fe13fa938c6237be9af7e1f60e2ed46";
+    sha256 = "sha256:11061jsvwb0kqw49zjmdv34zmysz28aj0nx3cfhlrpw6wzv1bp8r";
+  };
+in with (import pinnedPkgs { }).pkgs;
+let
+  pkg = haskellngPackages.callPackage ({ mkDerivation, base, turtle, stdenv }:
+    mkDerivation {
+      pname = "turtle-example";
+      version = "0.0.0.0";
+      src = ./.;
+      isLibrary = false;
+      isExecutable = true;
+      buildDepends = [ base turtle ];
+      license = stdenv.lib.licenses.bsd2;
+    }) { };
+in pkg.env
